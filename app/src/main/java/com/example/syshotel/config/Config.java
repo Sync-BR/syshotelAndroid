36cffeb5889226e64734c79d53469153871fa945
eduardo <eduardofreitasfr@gmail.com>
2025-05-13 23:25:08 -0300

    feat: adicionado url da rota

--- app/src/main/java/com/example/syshotel/config/Config.java
+++ app/src/main/java/com/example/syshotel/config/Config.java
@@ -7,7 +7,7 @@ public class Config {
 
     static class ConfigurationRoute {
         public static String protocol = "http";
-        public static String route = "186.247.89.58";
+        public static String route = "127.0.0.1";
         public static Integer port = 8080;
     }
 
